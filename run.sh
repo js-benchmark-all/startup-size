085d6759074742ae9480446f8d2ef4d4c42ced36
aquapi <aquaplmc@gmail.com>
2025-12-21 00:13:31 +0700

    fix script

--- run.sh
+++ run.sh
@@ -2,19 +2,19 @@
 bun task:build
 
 fnm use 22 --install-if-missing
-bun task:start node
+bun task:start --runtime node
 
 fnm use 23 --install-if-missing
-bun task:start node
+bun task:start --runtime node
 
 fnm use 24 --install-if-missing
-bun task:start node
+bun task:start --runtime node
 
 bun upgrade
-bun task:start bun
+bun task:start --runtime bun
 
 deno upgrade
-bun task:start deno
+bun task:start --runtime deno
 
 git add .
 git commit -m "Update results"
