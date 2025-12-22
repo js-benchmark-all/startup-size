6d1d8fa5f34ea8ae07994b0014864623552adfc6
aquapi <aquaplmc@gmail.com>
2025-12-22 15:48:02 +0700

    fix tb

--- runtime/src/validators/assert-simple/typebox.case.ts
+++ runtime/src/validators/assert-simple/typebox.case.ts
@@ -1,17 +1,6 @@
 import { register } from './spec.ts';
-import { Check } from './typebox/aot.ts';
 import schema from './typebox/schema.ts';
-import { Compile } from 'typebox/compile';
 import Value from 'typebox/value';
-
-const assert = Compile(schema);
-register('typebox (jit)', (o) => {
-  if (!assert.Check(o)) throw new Error();
-});
-
-register('typebox (aot)', (o) => {
-  if (!Check(o)) throw new Error();
-});
 
 register('typebox', (o) => {
   if (!Value.Check(schema, o)) throw new Error();
