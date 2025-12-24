0957da03cfd203bb05060243d1dba355436ad00e
aquapi <aquaplmc@gmail.com>
2025-12-24 12:18:46 +0700

    update

---
+++ runtime/src/async/async-mutex.case.ts
@@ -0,0 +1,12 @@
+import concurrency, { TASKS_COUNT } from './concurrency.spec.ts';
+import { Mutex, Semaphore } from 'async-mutex';
+
+await concurrency('async-mutex', (task, c) => {
+  if (c === 1) {
+    const mu = new Mutex()
+    return () => mu.runExclusive(task);
+  }
+
+  const sem = new Semaphore(c);
+  return () => sem.runExclusive(task);
+});
