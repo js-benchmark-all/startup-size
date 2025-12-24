09fe8d4de40b6ce24aa7b3ba68ba1b068ae40b9c
aquapi <aquaplmc@gmail.com>
2025-12-24 13:02:07 +0700

    pin version

--- startup/src/web-frameworks/wintercg/elysia (jit).case.ts
+++ startup/src/web-frameworks/wintercg/elysia (jit).case.ts
@@ -3,6 +3,6 @@ import Elysia from 'elysia';
 export const app = new Elysia()
   .get('/', 'Hi')
   .get('/user/:id', (c) => c.params.id)
-  .post('/body', (c) => c.body, { type: 'json' });
+  .post('/body', (c) => c.body);
 
 app.fetch(new Request('http://127.0.0.1'));
