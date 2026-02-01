1b3a388f7f5f234307efeea4523da99699838f07
aquapi <aquaplmc@gmail.com>
2026-02-01 23:18:56 +0700

    update

--- startup/src/web-frameworks/wintercg/hono.case.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono';
-
-export const app = new Hono()
-  .get('/', (c) => c.body('Hi'))
-  .get('/user/:id', (c) => c.body(c.req.param('id')))
-  .post('/body', async (c) => c.json(await c.req.json()));
-
-app.fetch(new Request('http://127.0.0.1'));
