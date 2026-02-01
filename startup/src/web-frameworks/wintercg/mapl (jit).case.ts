1b43f3ea83e65d5baad40cae4af9a382fc8159b0
aquapi <aquaplmc@gmail.com>
2026-02-01 17:08:50 +0700

    update

--- startup/src/web-frameworks/wintercg/mapl (jit).case.ts
+++ startup/src/web-frameworks/wintercg/mapl (jit).case.ts
@@ -1,23 +1,26 @@
-import { inject, router, send } from '@mapl/web';
-import { request, compiler } from '@mapl/web/generic';
+import { router, send } from '@mapl/web';
+import { request, build } from '@mapl/web/generic';
+import { getDependency } from 'runtime-compiler';
 
 export const app = {
-  fetch: compiler.build(
-    router.init(
-      [],
-      [
-        router.get(
-          '/',
-          send.raw(() => 'Hi'),
-        ),
-        router.get(
-          '/user/*',
-          send.raw((id) => id),
-        ),
-        router.post('/body', send.json(inject([request], async (req) => req.json()))),
-      ],
+  fetch: getDependency(
+    build(
+      router.init(
+        [],
+        [
+          router.get(
+            '/',
+            send.raw(() => 'Hi'),
+          ),
+          router.get(
+            '/user/*',
+            send.raw((id) => id),
+          ),
+          router.post('/body', send.json(async (req) => req.json(), request)),
+        ],
+      ),
     ),
-  )(),
+  )
 };
 
 app.fetch(new Request('http://127.0.0.1'));
