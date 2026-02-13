6c1991e49996b4aba60729b0d326829582ae37a0
aquapi <aquaplmc@gmail.com>
2026-02-13 10:34:10 +0700

    update

--- runtime/src/url-routers/mapl-tree.case.ts
+++ runtime/src/url-routers/mapl-tree.case.ts
@@ -1,16 +1,17 @@
-import { createRouter, insertItem } from '@mapl/router/method';
-import { createStaticMap } from '@mapl/router/path';
-import type { Node } from '@mapl/router/tree/node';
+import { createRouter, insertItem } from "@mapl/router/method";
+import { createStaticMap } from "@mapl/router/path";
+import type { Node } from "@mapl/router/tree/node";
 
-import spec from './.spec.ts';
+import spec from "./.spec.ts";
 
 {
   type Handler = (() => string) | ((params: string[]) => string);
 
+  let PARAMS!: string[];
+
   const _matchNode = (
     node: Node<Handler>,
     path: string,
-    params: string[],
     start: number,
   ): Handler | null => {
     const part = node[0];
@@ -28,33 +29,53 @@ import spec from './.spec.ts';
       if (node[2] != null) {
         const child = node[2][path.charCodeAt(start)];
         if (child != null) {
-          const match = _matchNode(child, path, params, start);
+          const match = _matchNode(child, path, start);
           if (match != null) return match;
         }
       }
 
       // Check for parameters
       if (node[3] != null) {
-        const endIdx = path.indexOf('/', start);
+        const paramNode = node[3];
 
-        if (endIdx === -1) {
-          if (node[3][1] != null) {
-            params.push(path.slice(start));
-            return node[3][1];
+        if (paramNode[0] == null) {
+          if (!path.includes("/", start)) {
+            PARAMS.push(path.slice(start));
+            return paramNode[1];
           }
-        } else if (endIdx > start && node[3][0] != null) {
-          params.push(path.slice(start, endIdx));
+        } else {
+          const endIdx = path.indexOf("/", start);
 
-          const match = _matchNode(node[3][0], path, params, endIdx);
-          if (match != null) return match;
+          if (paramNode[1] == null) {
+            if (endIdx > start) {
+              PARAMS.push(path.slice(start, endIdx));
 
-          params.pop();
+              const match = _matchNode(paramNode[0], path, endIdx);
+              if (match != null) return match;
+
+              PARAMS.pop();
+            }
+          } else {
+            if (endIdx === -1) {
+              if (paramNode[1] != null) {
+                PARAMS.push(path.slice(start));
+                return paramNode[1];
+              }
+            } else if (endIdx > start) {
+              PARAMS.push(path.slice(start, endIdx));
+
+              const match = _matchNode(paramNode[0], path, endIdx);
+              if (match != null) return match;
+
+              PARAMS.pop();
+            }
+          }
         }
       }
 
       // Wildcard
       if (node[4] != null) {
-        params.push(path.slice(start));
+        PARAMS.push(path.slice(start));
         return node[4];
       }
     }
@@ -68,21 +89,23 @@ import spec from './.spec.ts';
     insertItem(router, method, path, fn);
   };
 
-  insert('GET', '/user', () => '0');
-  insert('GET', '/user/comments', () => '1');
-  insert('GET', '/user/avatar', () => '2');
-  insert('GET', '/user/lookup/username/*', (params) => '3' + params[0]);
-  insert('GET', '/user/lookup/email/*', (params) => '4' + params[0]);
-  insert('GET', '/event/*', (params) => '5' + params[0]);
-  insert('GET', '/event/*/comments', (params) => '6' + params[0]);
-  insert('POST', '/event/*/comment', (params) => '7' + params[0]);
-  insert('GET', '/map/*/event', (params) => '8' + params[0]);
-  insert('GET', '/status', () => '9');
-  insert('GET', '/very/deeply/nested/route/hello/there', () => '10');
-  insert('GET', '/static/**', (params) => '11' + params[0]);
+  insert("GET", "/user", () => "0");
+  insert("GET", "/user/comments", () => "1");
+  insert("GET", "/user/avatar", () => "2");
+  insert("GET", "/user/lookup/username/*", (params) => "3" + params[0]);
+  insert("GET", "/user/lookup/email/*", (params) => "4" + params[0]);
+  insert("GET", "/event/*", (params) => "5" + params[0]);
+  insert("GET", "/event/*/comments", (params) => "6" + params[0]);
+  insert("POST", "/event/*/comment", (params) => "7" + params[0]);
+  insert("GET", "/map/*/event", (params) => "8" + params[0]);
+  insert("GET", "/status", () => "9");
+  insert("GET", "/very/deeply/nested/route/hello/there", () => "10");
+  insert("GET", "/static/**", (params) => "11" + params[0]);
 
-  const methodMap: Map<string, [Map<string, Handler>, Node<Handler>] | [Map<string, Handler>]> =
-    new Map();
+  const methodMap: Map<
+    string,
+    [Map<string, Handler>, Node<Handler>] | [Map<string, Handler>]
+  > = new Map();
   for (const method in router) {
     const methodRouter = router[method];
     methodMap.set(
@@ -93,21 +116,21 @@ import spec from './.spec.ts';
     );
   }
 
-  spec('mapl (tree)', (o) => {
+  spec("mapl (tree)", (o) => {
     const tmp = methodMap.get(o.method);
-    if (typeof tmp !== 'undefined') {
+    if (typeof tmp !== "undefined") {
       const match = tmp[0].get(o.url);
       if (match != null)
         // @ts-ignore
         return match();
 
       if (tmp.length > 1) {
-        const params: string[] = [];
-        const dmatch = _matchNode(tmp[1]!, o.url, params, 0);
-        if (dmatch != null) return dmatch(params);
+        PARAMS = [];
+        const dmatch = _matchNode(tmp[1]!, o.url, 0);
+        if (dmatch != null) return dmatch(PARAMS);
       }
     }
 
-    return '';
+    return "";
   });
 }
